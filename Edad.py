2dcb56e3b991df667895b077b259d4cd20f9658f
super usuario vagrant <vagrant@vagrant.vagrant-debian-amd64>
2017-10-04 15:53:29 -0500

    se modifico el yaml

--- Edad.py
+++ Edad.py
@@ -2,22 +2,22 @@
 #ITS LIT
 class Concurrencias():
 
-	def obtener_diccionario(self,texto):
-	#split each sentences at '.'
-	lista = texto.lower().split('.')
-	#declare empty dictionary for the counter
-	diccionario = {}
-	listaAux = []
+    def obtener_diccionario(self,texto):
+    #split each sentences at '.'
+    lista = texto.lower().split('.')
+    #declare empty dictionary for the counter
+    diccionario = {}
+    listaAux = []
 
-	for sentence in lista:
-		words = sentence.split()
-		for word in words:
-			index = words.index(word)
-			listaAux.append(index)
-			if word not in diccionario:
-				diccionario[word] = list()
-				diccionario[word].append(index)
-			else:
-				diccionario[word]= list()
-				diccionario[word].append(index)
-	return diccionario
+    for sentence in lista:
+        words = sentence.split()
+        for word in words:
+            index = words.index(word)
+            listaAux.append(index)
+            if word not in diccionario:
+                diccionario[word] = list()
+                diccionario[word].append(index)
+            else:
+                diccionario[word]= list()
+                diccionario[word].append(index)
+    return diccionario
